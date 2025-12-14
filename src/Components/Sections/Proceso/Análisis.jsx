1ae5e988f7db9dfe9c2f5ccaeea1ee1a481b4dda
Agustin Indarte <aindarte111@gmail.com>
2025-12-14 19:19:28 -0300

    Responsive Análisis-Section

--- src/Components/Sections/Proceso/Análisis.jsx
+++ src/Components/Sections/Proceso/Análisis.jsx
@@ -1,34 +1,42 @@
 import { Row, Col } from 'react-bootstrap'
-import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react'
 import './Análisis.css'
-import BtnVerMás from '../../Buttons/BtnVerMás/BtnVerMás';
+import BtnVerMás from '../../Buttons/BtnVerMás/BtnVerMás'
 
 function Análisis() {
   return (
+    <section className="Analisis-Section">
+      <Row className="analisis-row w-100">
 
-  <section className='Análisis-Section d-flex flex-column align-items-center'>
-      <Row className='w-100 align-items-stretch justify-content-center vh-100'>
-        <Col md={6} className='d-flex justify-content-center align-items-center h-100 '>
-          <DotLottieReact
-            className='lottie-analisis'
-            src="https://lottie.host/067270ec-dc43-4672-a87b-8d3180e5fee8/CPDGbcRch3.lottie"
-            loop
-            autoplay
-          />
+        {/* CONTENEDOR LOTTIE */}
+        <Col md={6} className="analisis-lottie">
+          <div className="lottie-wrapper">
+            <DotLottieReact
+              className="lottie-analisis"
+              src="https://lottie.host/067270ec-dc43-4672-a87b-8d3180e5fee8/CPDGbcRch3.lottie"
+              loop
+              autoplay
+            />
+          </div>
         </Col>
 
-        <Col md={6} className='d-flex flex-column justify-content-center align-items-start text-start h-100'>
-          <h3 className='analisis-title text-white fw-bolder'>ANÁLISIS</h3>
-          <p className='analisis-text text-white'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam nam quasi beatae. Magnam molestias voluptatibus nemo, cumque magni et commodi earum eligendi accusamus.
+        {/* CONTENIDO */}
+        <Col md={6} className="analisis-content">
+          <h3 className="analisis-title text-white fw-bolder">
+            ANÁLISIS
+          </h3>
+
+          <p className="analisis-text text-white">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Quisquam nam quasi beatae. Magnam molestias voluptatibus nemo,
+            cumque magni et commodi earum eligendi accusamus.
           </p>
+
           <BtnVerMás />
         </Col>
+
       </Row>
     </section>
-
-
-
   )
 }
 
